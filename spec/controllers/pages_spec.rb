debf8c15c724b2cb30cb3370d0c6fbf3b8d0329a
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-26 20:59:00 -0700

    Update readme
    Add Travis CI
    
    Specify Ruby version for Travis
    
    Remove skipped tests
    
    Add environment information to travis.yml
    
    Finish travis configuration

--- spec/controllers/pages_spec.rb
+++ spec/controllers/pages_spec.rb
@@ -6,13 +6,13 @@ describe PagesController do
     expect(response).to render_template(:index)
   end
 
-  xit 'about path is pages#about' do
-    get '/about'
-    expect(response).to render_template(:about)
-  end
-
-  xit 'who is in space? path is pages#who' do
-    get '/who_is_in_space'
-    expect(response).to render_template(:who_is_in_space)
-  end
+  # xit 'about path is pages#about' do
+  #   get '/about'
+  #   expect(response).to render_template(:about)
+  # end
+  #
+  # xit 'who is in space? path is pages#who' do
+  #   get '/who_is_in_space'
+  #   expect(response).to render_template(:who_is_in_space)
+  # end
 end
---
+++ spec/features/user_can_enter_zipcode_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "User can see next ISS flyover" do
+  before do
+    visit root_path
+  end
+
+  xit "after entering zipcode on homepage" do
+    
+  end
+end
